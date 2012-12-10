0105e8da64257fe9c3d872b918e9a7c82f1c5f1c
Matthew Varley <matthewkvarley@gmail.com>
2012-12-10 14:39:00 -0500

    Add unique identifier for Relationship

--- src/spiderman/Relationship.java
+++ src/spiderman/Relationship.java
@@ -3,8 +3,17 @@ package spiderman;
 /**
  * 
  * @author varley
- *
+ * 
  */
 public interface Relationship {
 
+	/**
+	 * This should return a unique identifier for the specific relationship.
+	 * 
+	 * For instance in {@link NamedRelationship}, each different type of
+	 * NamedRelationship should return a different string.
+	 * 
+	 * @return
+	 */
+	public String identifier();
 }
