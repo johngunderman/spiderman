f5496651e9b63a5e7ce3de703197f9ef035f789e
Matthew Varley <mkv13@case.edu>
2012-12-16 15:56:32 -0500

    Push some things out into the interface

--- src/storage/Node.java
+++ src/storage/Node.java
@@ -115,7 +115,7 @@ public class Node<T> implements java.lang.Comparable<Node<?>> {
 	}
 
 	public QueryPlan breadthFirst() {
-		return new QueryPlan(new BreadthFirstSearch(this, Integer.MAX_VALUE));
+		return breadthFirst(Integer.MAX_VALUE);
 	}
 
 	public QueryPlan breadthFirst(int maxDepth) {
@@ -123,7 +123,7 @@ public class Node<T> implements java.lang.Comparable<Node<?>> {
 	}
 
 	public QueryPlan depthFirst() {
-		return new QueryPlan(new DepthFirstSearch(this, Integer.MAX_VALUE));
+		return depthFirst(Integer.MAX_VALUE);
 	}
 
 	public QueryPlan depthFirst(int maxDepth) {
