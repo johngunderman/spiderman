10badd6131bd2671c445a9a9bdcc8f1680162b44
John Gunderman <johngunderman@gmail.com>
2012-12-18 19:39:45 -0500

    Starting an evaluator for the second test

--- src/test/TestDB2.java
+++ src/test/TestDB2.java
@@ -8,6 +8,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import evaluator.Evaluable;
+
 import spiderman.Direction;
 import spiderman.NamedRelationship;
 import storage.BasicGraph;
@@ -43,14 +45,39 @@ public class TestDB2 {
 		Node<String> chicago = g.addNode("Chicago");
 		Node<String> sally = g.addNode("Sally");
 		
-		g.addRelationship(new NamedRelationship("HATES"), Direction.Directed, frank, bill);
-		g.addRelationship(new NamedRelationship("DATING"), Direction.Undirected, bill, sally);
-		g.addRelationship(new NamedRelationship("EMPLOYS"), Direction.Directed, sally, hank);
-		g.addRelationship(new NamedRelationship("LEASES_TO"), Direction.Directed, hank, mike);
-		g.addRelationship(new NamedRelationship("LIVES_IN"), Direction.Directed, mike, chicago);
-		g.addRelationship(new NamedRelationship("LIVES_IN"), Direction.Directed, hank, chicago);
-		g.addRelationship(new NamedRelationship("LIVES_IN"), Direction.Directed, frank, chicago);
+		NamedRelationship hates = new NamedRelationship("HATES");
+		NamedRelationship dating = new NamedRelationship("DATING");
+		NamedRelationship employs = new NamedRelationship("EMPLOYS");
+		NamedRelationship leases_to = new NamedRelationship("LEASES_TO");
+		NamedRelationship lives_in = new NamedRelationship("LIVES_IN");
+
+
+
 		
+		g.addRelationship(hates, Direction.Directed, frank, bill);
+		g.addRelationship(dating, Direction.Undirected, bill, sally);
+		g.addRelationship(employs, Direction.Directed, sally, hank);
+		g.addRelationship(leases_to, Direction.Directed, hank, mike);
+		g.addRelationship(lives_in, Direction.Directed, mike, chicago);
+		g.addRelationship(lives_in, Direction.Directed, hank, chicago);
+		g.addRelationship(lives_in, Direction.Directed, frank, chicago);
+		
+		
+		g.hasRelation(hates).evaluate(new Evaluable() {
+
+			@Override
+			public <T> boolean evaluateNode(T data) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public boolean evaluateNode(Node<?> data) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+		});
 
 
 		
